2aa7ed605e794d14d11879e7e9d7d7991298e814
megdlawit anbessie <megdalenea@gmail.com>
2022-03-10 20:49:50 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-init main(void) {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
